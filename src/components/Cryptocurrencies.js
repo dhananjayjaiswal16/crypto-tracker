c6caf6d1f299d7ab908e70254a5a22c14279f9a3
Dhananjay <dhananjayman11@gmail.com>
2022-01-17 11:35:52 +0530

    chore: lint error fix

--- src/components/Cryptocurrencies.js
+++ src/components/Cryptocurrencies.js
@@ -6,51 +6,51 @@ import { useGetCryptosQuery } from '../sevices/cryptoApi';
 import millify from 'millify';
 
 const Cryptocurrencies = ({ simplified }) => {
-    const count = (simplified === undefined) ? 100 : 10;
-    const { data: cryptoData, isFetching } = useGetCryptosQuery(count);
-    const cryptoCurrencyData = cryptoData?.data?.coins;
-    const [cryptos, setCryptos] = useState(cryptoCurrencyData);
-    const [search, setSearch] = useState('');
-    // console.log("cryptos", cryptos);
+  const count = (simplified === undefined) ? 100 : 10;
+  const { data: cryptoData, isFetching } = useGetCryptosQuery(count);
+  const cryptoCurrencyData = cryptoData?.data?.coins;
+  const [cryptos, setCryptos] = useState(cryptoCurrencyData);
+  const [search, setSearch] = useState('');
+  // console.log("cryptos", cryptos);
 
-    useEffect(() => {
+  useEffect(() => {
 
-        const filteredData = cryptoData?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()));
+    const filteredData = cryptoData?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()));
 
-        setCryptos(filteredData);
-    }, [cryptoData, search]);
+    setCryptos(filteredData);
+  }, [cryptoData, search]);
 
-    if (isFetching) return <Loader />;
-    return (
-        <>
-            {!simplified && <div className='search-crypto'>
-                <Input placeholder="Search..." onChange={(e) => setSearch(e.target.value)} />
-            </div>}
-            <Row className='crypto-card-container' gutter={[36, 36]}>
-                {cryptos?.map((coin) => (
-                    <Col className='crypto-card' xs={24} sm={12} md={8} lg={6} key={coin?.id}>
-                        <Link to={`/crypto/${coin?.id}`}>
-                            <Card title={`${coin?.rank}) ${coin?.name}`} extra={<img src={coin?.iconUrl} className='crypto-image' />} hoverable>
-                                <p>Price: ${(coin?.price < 0.01) ? (
-                                    millify(coin?.price, {
-                                        precision: 6
-                                    })
-                                ) : (
-                                    millify(coin?.price, {
-                                        precision: 2
-                                    })
-                                )
-                                }</p>
-                                <p>Market Cap: {millify(coin?.marketCap)}</p>
-                                <p>Daily Change: {millify(coin?.change)}%</p>
-                                <p>Circulating Supply: {coin?.circulatingSupply && millify(coin?.circulatingSupply)}</p>
-                            </Card>
-                        </Link>
-                    </Col>
-                ))}
-            </Row>
-        </>
-    )
+  if (isFetching) return <Loader />;
+  return (
+    <>
+      {!simplified && <div className='search-crypto'>
+        <Input placeholder="Search..." onChange={(e) => setSearch(e.target.value)} />
+      </div>}
+      <Row className='crypto-card-container' gutter={[36, 36]}>
+        {cryptos?.map((coin) => (
+          <Col className='crypto-card' xs={24} sm={12} md={8} lg={6} key={coin?.id}>
+            <Link to={`/crypto/${coin?.id}`}>
+              <Card title={`${coin?.rank}) ${coin?.name}`} extra={<img src={coin?.iconUrl} className='crypto-image' alt='crypto-img' />} hoverable>
+                <p>Price: ${(coin?.price < 0.01) ? (
+                  millify(coin?.price, {
+                    precision: 6
+                  })
+                ) : (
+                  millify(coin?.price, {
+                    precision: 2
+                  })
+                )
+                }</p>
+                <p>Market Cap: {millify(coin?.marketCap)}</p>
+                <p>Daily Change: {millify(coin?.change)}%</p>
+                <p>Circulating Supply: {coin?.circulatingSupply && millify(coin?.circulatingSupply)}</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
 }
 
 export default Cryptocurrencies;
